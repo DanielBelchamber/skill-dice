0e52154ca5aabd0ca1abc50750fc4259b5e50d74
Daniel Belchamber <daniel.belchamber@gmail.com>
2024-02-14 00:47:18 -0500

    feat: die backgrounds

--- src/components/SkillDie.vue
+++ src/components/SkillDie.vue
@@ -35,6 +35,51 @@ const fillColor = computed(() => {
 
 <template>
   <svg class="SkillDie" :width="size" :height="size" viewBox="0 0 100 100">
-    <circle cx="50" cy="50" r="50" :fill="fillColor" />
+    <rect v-if="dieType === 'Master'" x="14" y="14" width="72" height="72" :fill="fillColor" />
+
+    <g v-if="dieType === 'Expert'">
+      <!-- background -->
+      <polygon points="10,25 50,5 90,25 90,75 50,95 10,75" :fill="fillColor" />
+      <!-- line hints -->
+      <line x1="50" y1="5" x2="90" y2="75" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="10" y1="75" x2="90" y2="75" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="10" y1="75" x2="50" y2="5" stroke="white" stroke-width="2" opacity="0.2" />
+      <!-- hint cover -->
+      <circle cx="50" cy="50" r="32" :fill="fillColor" />
+    </g>
+
+    <g v-if="dieType === 'Adept'">
+      <!-- background -->
+      <polygon
+        points="50,5 76.43,13.59 92.8,36.09 92.8,63.91 76.43,86.41 50,95 23.57,86.41 7.2,63.91 7.2,36.09 23.57,13.59"
+        :fill="fillColor" />
+      <!-- line hints -->
+      <line x1="50" y1="5" x2="50" y2="50" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="92.8" y1="36.09" x2="50" y2="50" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="76.43" y1="86.41" x2="50" y2="50" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="23.57" y1="86.41" x2="50" y2="50" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="7.2" y1="36.09" x2="50" y2="50" stroke="white" stroke-width="2" opacity="0.2" />
+      <!-- hint cover -->
+      <circle cx="50" cy="50" r="35" :fill="fillColor" />
+    </g>
+
+    <g v-if="dieType === 'Novice'">
+      <!-- background -->
+      <polygon points="6.7,25 50,0 93.3,25 93.3,75 50,100 6.7,75" :fill="fillColor" />
+      <!-- line hints -->
+      <line x1="50" y1="0" x2="50" y2="20" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="6.7" y1="25" x2="50" y2="20" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="93.3" y1="25" x2="50" y2="20" stroke="white" stroke-width="2" opacity="0.2" />
+
+      <line x1="93.3" y1="75" x2="75.98" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="93.3" y1="25" x2="75.98" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="50" y1="100" x2="75.98" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+
+      <line x1="6.7" y1="75" x2="24.02" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="6.7" y1="25" x2="24.02" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+      <line x1="50" y1="100" x2="24.02" y2="65" stroke="white" stroke-width="2" opacity="0.2" />
+      <!-- hint cover -->
+      <circle cx="50" cy="50" r="38" :fill="fillColor" />
+    </g>
   </svg>
 </template>
