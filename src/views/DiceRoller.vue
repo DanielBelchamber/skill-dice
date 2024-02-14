afd3d3b882a7a4a554ea28bf588d5d17aff2a6ec
Daniel Belchamber <daniel.belchamber@gmail.com>
2024-02-13 21:38:46 -0500

    refactor: minimal app

---
+++ src/views/DiceRoller.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Skill Dice</h1>
+
+    <div class="dice-selector"></div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+}
+
+h1 {
+  margin: 3rem 0;
+  font-size: 3rem;
+  font-weight: 600;
+  text-align: center;
+}
+
+.dice-selector {
+  height: 200px;
+  width: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
